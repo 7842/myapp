dc5cc05a31d0c5b7305c3907fcc3ad457d767eea
Chanon Navajongpun <chanon.na@ku.th>
2022-07-05 21:10:06 +0700

    bring back src/myapp/Cat.java from commit b520015

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,19 +1,4 @@
 package myapp;
 
 public class Cat {
-
-    private String name ;
-
-    public Cat(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
